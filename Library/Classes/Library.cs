4d4ee8b5c1ad74a0b7a1b8277b233dfd4020f974
miriam silva <miriam-silva@live.com>
2021-07-21 17:38:43 -0700

    Added Classes, Interfaces & Starter Code

---
+++ Library/Classes/Library.cs
@@ -0,0 +1,13 @@
+﻿using Library.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library.Classes
+{
+    class Library : ILibrary
+    {
+    }
+}
